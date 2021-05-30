683b01dbd018e85f1b5605920b985c5f72172e02
N1R0 <85079850+ncrcodes@users.noreply.github.com>
2021-05-31 00:01:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2021 DevelopersSupportAR
+// هتشيل اي حقوق هنيكك
+
 require("./src/website/server");
 const Discord = require("discord.js");
 const { Collection, Client, MessageEmbed } = require("discord.js");
