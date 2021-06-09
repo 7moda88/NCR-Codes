8444efbbd8c6479e38335d345b29024ec5b08f1b
N1R0 <85079850+ncrcodes@users.noreply.github.com>
2021-06-09 18:39:20 +0200

    Update server.js

--- src/website/server.js
+++ src/website/server.js
@@ -2,19 +2,10 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const chalk = require("chalk");
-const cookies = require("cookies");
 const rootRoutes = require("./routes/root-routes");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-
+const expressUrl = require("../config/bot").expressUrl;
 app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(cookies.express("a", "b", "c"));
-
-app.use(cookies.express("a", "b", "c"));
 
 app.use(express.static(`${__dirname}/assets`));
 app.locals.basedir = `${__dirname}/assets`;
@@ -29,11 +20,11 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(chalk.red.bold("Server Is On")));
 
 setInterval(() => {
-  http.get(`http://ncr-codes.glitch.me/`);
+  http.get(`http://${expressUrl}`);
 }, 280000);
 
 const request = require("node-superfetch");
 setInterval(async () => {
-  await request.get("http://ncr-codes.glitch.me/");
-  await request.get("https://ncr-codes.glitch.me/");
+  await request.get(`http://${expressUrl}`);
+  await request.get(`https://${expressUrl}`);
 }, 5 * 60 * 1000);
