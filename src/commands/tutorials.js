3f25e9551a84abc5b9ac86dc64efd87ce47c21de
N1R0 <85079850+ncrcodes@users.noreply.github.com>
2021-06-10 02:01:41 +0200

    Bot Commands 💡🧨

--- src/commands/tutorials.js
+++ src/commands/tutorials.js
@@ -133,6 +133,48 @@ module.exports = {
                         )
                     );
                 }
+            } else if (args[1] === "py" || "python") {
+                if (lang2 == "english") {
+                    message.lineReply(
+                        new MessageEmbed()
+                        .setTitle(`Videos ${client.emotes.success}`)
+                        .setColor("YELLOW")
+                        .setDescription(
+                            `**[#0](https://www.youtube.com/watch?v=aWr4KD7YzJo)
+                            [#1](https://www.youtube.com/watch?v=AYmCseFw5cs)
+                            [#2](https://www.youtube.com/watch?v=JXDzWrc46Ls)
+                            [#3](https://www.youtube.com/watch?v=7XS92R-Qmq4)
+                            [#4](https://www.youtube.com/watch?v=V-5AGfl9rks)
+                            [#5](https://www.youtube.com/watch?v=sfZGQMA1f0c)**`
+                        )
+                        .setFooter(`Request By: ${message.author.tag}`)
+                        .setTimestamp()
+                    );
+                } else if (lang2 == "arabic") {
+                    message.lineReply(
+                        new MessageEmbed()
+                        .setTitle(`فيديوه ${client.emotes.success}`)
+                        .setColor("YELLOW")
+                        .setDescription(
+                            `**[#0](https://www.youtube.com/watch?v=aWr4KD7YzJo)
+                            [#1](https://www.youtube.com/watch?v=AYmCseFw5cs)
+                            [#2](https://www.youtube.com/watch?v=JXDzWrc46Ls)
+                            [#3](https://www.youtube.com/watch?v=7XS92R-Qmq4)
+                            [#4](https://www.youtube.com/watch?v=V-5AGfl9rks)
+                            [#5](https://www.youtube.com/watch?v=sfZGQMA1f0c)**`
+                        )
+                        .setFooter(`Request By: ${message.author.tag}`)
+                        .setTimestamp()
+                    );
+                } else {
+                    return message.lineReply(
+                        new MessageEmbed()
+                        .setColor("RED")
+                        .setDescription(
+                            `**> \`-\` The bot does not work? it's ok just type \`${prefix}set-lang english\`\n> \`-\` البوت لا يعمل؟ يرجى كتابة \`${prefix}set-lang arabic\`**`
+                        )
+                    );
+                }
             } else {
                 if (lang2 == "english") {
                     message.lineReply(
@@ -140,7 +182,7 @@ module.exports = {
                         .setTitle(`Error ${client.emotes.error}`)
                         .setColor("RED")
                         .setDescription(
-                            `**${client.emotes.error} | Oly \`js\`, \`quick.db\` Is Avelble**`
+                            `**${client.emotes.error} | Oly \`js\`, \`quick.db\`, \`py\` Is Avelble**`
                         )
                         .setFooter(`Request By: ${message.author.tag}`)
                         .setTimestamp()
@@ -150,7 +192,7 @@ module.exports = {
                         new MessageEmbed()
                         .setTitle(`Error ${client.emotes.error}`)
                         .setColor("RED")
-                        .setDescription(`**${client.emotes.error} | فقط ال \`js\`, \`quick.db\` متاح**`)
+                        .setDescription(`**${client.emotes.error} | فقط ال \`js\`, \`quick.db\`, \`py\` متاح**`)
                         .setFooter(`Request By: ${message.author.tag}`)
                         .setTimestamp()
                     );
