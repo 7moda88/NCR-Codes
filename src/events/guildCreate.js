96190d4dbfa71abb11fb4626c887b6d2fe839018
N1R0 <85079850+ncrcodes@users.noreply.github.com>
2021-08-19 02:10:16 +0200

    Bot Events 🎯🎲

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = async function(client, guild) {
   await db.set(`langguild_${guild.id}`, "arabic");
-  await db.set(`prefix_${guild.id}`, "/");
+  await db.set(`prefix_${guild.id}`, client.config.prefix);
   var channel = client.channels.cache.find(c => c.id === "812817973802827806");
   var join = new MessageEmbed()
     .setColor("#2F3136")
